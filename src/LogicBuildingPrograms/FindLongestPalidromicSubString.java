ee7bd6ef4222e1d9cc3d98811fee50291b13cbb5
Prashanth N <imprash4u@gmail.com>
2025-01-24 23:42:29 +0530

    Longest Palidromic Substring

---
+++ src/LogicBuildingPrograms/FindLongestPalidromicSubString.java
@@ -0,0 +1,14 @@
+package LogicBuildingPrograms;
+
+public class FindLongestPalidromicSubString {
+
+    public static void main(String[] args) {
+
+        // subSequence: mainitains the order but can be consecutive or can't be also
+        // it should maintain the order
+        // abc, a ,b,c,ab,ac,bc,abc,""
+        // from above subSquence list we can see that bc is ac is there but not ba cb ca because it won't maintain
+        // the order n elements can generate 2pown subsequences
+
+    }
+}
